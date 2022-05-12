9d8c521dbc5a87d6bd6cf6e0d726982651dd3c42
Hoffmann-Ferreira <andreihf@outlook.com>
2022-05-11 23:32:11 -0300

    feat: config pastas, portas e pacotes

---
+++ index.js
@@ -0,0 +1,18 @@
+import express  from "express";
+import path from "path";
+
+const __dirname = path.resolve(path.dirname(''));
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+const port = 3005;
+
+app.listen(port, () =>{
+    console.log(`rodando na porta ${port}`)
+})
+
+
+
