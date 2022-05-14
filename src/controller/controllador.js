694b6e97378e34b0d6c4e0c5fd6bc6ad8f4d3af0
Hoffmann-Ferreira <andreihf@outlook.com>
2022-05-13 22:02:05 -0300

    feat: add tarefas edições em págians

--- src/controller/controllador.js
+++ src/controller/controllador.js
@@ -1,11 +1,15 @@
 import { connection } from "../database/connection.js";
 import { tarefas } from "../model/tarefas.js";
 
-export const getIndex = async(req, res) =>{
-    const alunos = await connection.query("SELECT * FROM tarefas", {
-        model: tarefas
-    })
-    res.render("index.ejs")
-}
+export const getIndex = async (req, res) => {
+  try {
+    const monstraTarefas = await tarefas.findAll();
+    res.render("index.ejs",{
+        monstraTarefas
+    });
+  } catch (error) {
+    res.send(error.message);
+  }
+};
 
 
